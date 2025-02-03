f1caec0594563fb5cabdae8975db46c666383d29
Gabriel Ladeira <gabrielxladeira@gmail.com>
2025-02-02 22:11:39 -0300

    fix health check controller

--- tests/application/controllers/test_health_check_controller.py
+++ tests/application/controllers/test_health_check_controller.py
@@ -4,10 +4,10 @@ import pytest
 class TestHealthCheckController:
     @pytest.fixture(autouse=True)
     def setup(self, api_client) -> None:
-        self.api_client = api_client
+        self._api_client = api_client
 
     def test_health_check_success(self) -> None:
-        response = self.api_client.get("/health-check")
+        response = self._api_client.get("/health-check")
 
         assert response.status_code == 200
         assert response.json() == {"success": True, "message": "Service is running successfully"}
